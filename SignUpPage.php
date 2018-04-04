8ba45658e4a78d0d29e16fed77595dc08352e887
Edwin Sanchez <es269@afsconnect2.njit.edu>
2018-04-04 18:53:20 -0400

    SignUpPage html, php, and css

---
+++ SignUpPage.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<link rel="stylesheet" type="text/css" href="SignUpPage.css">
+
+First Name :<?php echo $_GET["FirstName"];?><br>
+Last Name :<?php echo $_GET["LastName"];?><br>
+Email :<?php echo $_GET["Email"];?><br>
+Phone Number :<?php echo $_GET["Phone"];?><br>
+Birthday :<?php echo $_GET["Birthday"];?><br>
+Gender :<?php echo $_GET["Gender"];?><br>
+
+</body>
+</html>
